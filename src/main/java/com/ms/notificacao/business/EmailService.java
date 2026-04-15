b3a3b3eda8cd76cae0bc84850673dc47dc1852bb
LEONARDO ALVES DA SILVA <leonardoe5id@gmail.com>
2026-04-15 20:11:00 -0300

    adicionados metodos do servico envio de email

---
+++ src/main/java/com/ms/notificacao/business/EmailService.java
@@ -0,0 +1,49 @@
+package com.ms.notificacao.business;
+
+import com.ms.notificacao.dto.TarefasDTO;
+import com.ms.notificacao.infraestructure.exceptions.EmailException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
+    @Value("${envio.email.remetente}")
+    public String remetente;
+
+    @Value("${envio.email.nomeRemetente}")
+    private String nomeRemetente;
+
+    public void enviaEmail(TarefasDTO dto){
+
+        try{
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mensagem, true,
+                                                                        StandardCharsets.UTF_8.name());
+            Context context = new Context();
+            context.setVariable("nomeTarefa", dto.getNomeTarefa());
+            context.setVariable("dataEvento", dto.getDataEvento());
+            context.setVariable("descricao", dto.getDescricao());
+            String template = templateEngine.process("notificacao", context);
+            mimeMessageHelper.setText(template, true);
+            javaMailSender.send(mensagem);
+        } catch (MessagingException e){
+            throw new EmailException("Erro ao enviar email. ", e.getCause());
+        }
+    }
+}
---
+++ src/main/java/com/ms/notificacao/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,7 @@
+package com.ms.notificacao.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE,
+    NOTIFICADO,
+    CANCELADO
+}
